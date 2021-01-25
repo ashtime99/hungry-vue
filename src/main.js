8d4cbb32f564a5441ef4702ddcbb3bb3060ec2b2
ashtime99 <ashtime99@gmail.com>
2021-01-25 17:02:28 +0800

    first

--- src/main.js
+++ src/main.js
@@ -2,10 +2,20 @@ import Vue from 'vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import App from './App.vue'
+import router from './router'
+//axios
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+Vue.config.productionTip=false
+
+Vue.use(VueAxios, axios)
+
 
 Vue.use(ElementUI)
 
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
---
+++ src/router/index.js
@@ -0,0 +1,27 @@
+//引入vue
+import Vue from 'vue';
+//引入vue-router
+import VueRouter from 'vue-router';
+
+import Login  from '../views/Login.vue';
+//第三方库需要use一下才能用
+Vue.use(VueRouter)
+
+
+//定义routes路由的集合，数组类型
+const routes=[
+    {
+		path:'/',
+		name:'Login',
+		component:Login
+	}
+]
+
+//实例化VueRouter并将routes添加进去
+const router=new VueRouter({
+//ES6简写，等于routes：routes
+    routes
+});
+
+//抛出这个这个实例对象方便外部读取以及访问
+export default router
